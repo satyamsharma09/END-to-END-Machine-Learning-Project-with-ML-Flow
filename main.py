1e89c21ea50db45ee0d86e7c3905bf05138ca697
satyamsharma09 <satyamsharma0914@gmail.com>
2025-01-04 04:53:44 +0530

    Data Transform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from MLProject import logger
 from MLProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from MLProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from MLProject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,5 +14,29 @@ except Exception as e:
     logger. exception (e)
     raise e
 
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage (STAGE_NAME) started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage (STAGE_NAME) completed <<<<<<")
+except Exception as e:
+    logger. exception (e)
+    raise e
+
+logger.info("This is an info message")
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage (STAGE_NAME) started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage (STAGE_NAME) completed <<<<<<")
+except Exception as e:
+    logger. exception (e)
+    raise e
 
 logger.info("This is an info message")
