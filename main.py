5017db7c154bce3572fa6f84ff88932061c14ee7
satyamsharma09 <satyamsharma0914@gmail.com>
2025-01-04 05:32:21 +0530

    Model Training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from MLProject import logger
 from MLProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from MLProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from MLProject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from MLProject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -15,7 +16,6 @@ except Exception as e:
     raise e
 
 
-
 STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage (STAGE_NAME) started <<<<<<")
@@ -26,14 +26,23 @@ except Exception as e:
     logger. exception (e)
     raise e
 
-logger.info("This is an info message")
-
 
 STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>>> stage (STAGE_NAME) started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>> stage (STAGE_NAME) completed <<<<<<")
+except Exception as e:
+    logger. exception (e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Training Stage"
+try:
+    logger.info(f">>>>>> stage (STAGE_NAME) started <<<<<<")
+    modeltrainer = ModelTrainerTrainingPipeline()
+    modeltrainer.main()
     logger.info(f">>>>>> stage (STAGE_NAME) completed <<<<<<")
 except Exception as e:
     logger. exception (e)
